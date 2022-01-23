502fc289a6c9b1376c0cc2b5bb6ecad7771f06f5
Lokesh Patel <patellokesh05899@gmail.com>
2022-01-23 18:07:52 +0530

    Fetch client application to get owners

--- src/events/core/postLoginHooks/index.ts
+++ src/events/core/postLoginHooks/index.ts
@@ -6,7 +6,14 @@ export const postLoginHooks = new ClientEvent({
   type: 'ready',
   once: true,
   handler: async (client) => {
+    // Fetch client's application to get the owners
+    client.emit('debug', 'Fetching client application.');
+    await client.application.fetch();
+
+    // Register commands
     await registerCommands(client);
+
+    // Log we are ready
     Logger.info(`${client.user.username} is online.`);
   },
 });
